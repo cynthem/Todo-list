e796abe9dff8daab4acd8b6b885b91531af8430e
doozles411 <cindy.phelps01@gmail.com>
2022-08-16 16:24:08 -0700

    Store buttons in variables

--- src/modules/userInterface.js
+++ src/modules/userInterface.js
@@ -1,3 +1,5 @@
+// Hamburger menu
+const hamburgerMenu = document.querySelector('.header-menu');
 // Filter buttons
 const filterAll = document.querySelector('.all-btn');
 const filterToday = document.querySelector('.today-btn');
@@ -10,12 +12,25 @@ const itemCheckbox = document.querySelector('.item-check');
 const itemNotes = document.querySelector('.item-notes');
 const itemEdit = document.querySelector('.item-edit');
 const itemDelete = document.querySelector('.item-delete');
-// Form card buttons
+// Popup card buttons
     // Add-new form buttons
 const addExit = document.querySelector('.add-cancel');
-const addTodo = document.querySelector('.add-cancel');
-const addProject = document.querySelector('.add-cancel');
-const addSubmit = document.querySelector('.add-cancel');
+const addTodo = document.querySelector('.add-btn-todo');
+const addProject = document.querySelector('.add-btn-project');
+const addLowPriority = document.getElementById('new-low');
+const addMediumPriority = document.getElementById('new-medium');
+const addHighPriority = document.getElementById('new-high');
+const addTodoSubmit = document.querySelector('.new-todo-submit');
+const addProjectSubmit = document.querySelector('.new-project-submit');
+    // Notes buttons
+const notesExit = document.querySelector('.notes-cancel');
+    // Edit form buttons
+const editExit = document.querySelector('.edit-cancel');
+const editLowPriority = document.getElementById('edit-low');
+const editMediumPriority = document.getElementById('edit-medium');
+const editHighPriority = document.getElementById('edit-high');
+const editSubmit = document.querySelector('.edit-submit');
+
 
 
 
