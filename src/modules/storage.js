663f15a387aa8e005767cdb979da069d06d42042
doozles411 <cindy.phelps01@gmail.com>
2022-08-16 16:34:56 -0700

    Begin object storage logic

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,5 +1,22 @@
-export function storeInfo() {
-    function toDos(title, details, dueDate, priority, project) {
-        
+export const storeInfo = (() => {
+    let selectedProject = 'home';
+    function setSelectedProject(currentProject) {
+        selectedProject = currentProject;
     }
-}
+    function getSelectedProject() {
+        return selectedProject;
+    }
+    function createTodo(title, details, dueDate, priority, project, checked=false) {
+        return {
+            title,
+            details,
+            dueDate,
+            priority,
+            project,
+            checked
+        };
+    }
+    function addNewTodo(e, ) {
+        e.preventDefault();
+    }
+})
