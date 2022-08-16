9d81d6e9b339004fdc31b1aad8e413eb1a787810
doozles411 <cindy.phelps01@gmail.com>
2022-08-15 20:18:35 -0700

    Begin todos and projects functions

---
+++ src/modules/storage.js
@@ -0,0 +1,12 @@
+export function storeInfo() {
+    let currentProject = 'Kitchen renovation';
+    function getCurrentProject() {
+        return currentProject;
+    }
+    function setCurrentProject(newProject) {
+        currentProject = newProject;
+    }
+    function addProject(e) {
+        
+    }
+}
