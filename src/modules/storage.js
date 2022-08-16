3a960babe5fc681237f50d0a3e63251d07e7ff97
doozles411 <cindy.phelps01@gmail.com>
2022-08-16 16:52:12 -0700

    Create add new todo factory function

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,11 +1,15 @@
 export const storeInfo = (() => {
+
     let selectedProject = 'home';
+
     function setSelectedProject(currentProject) {
         selectedProject = currentProject;
     }
+
     function getSelectedProject() {
         return selectedProject;
     }
+
     function createTodo(title, details, dueDate, priority, project, checked=false) {
         return {
             title,
@@ -16,7 +20,18 @@ export const storeInfo = (() => {
             checked
         };
     }
-    function addNewTodo(e, ) {
+
+    function addNewTodo(e, /*todoList, display, overlay, form*/) {
+
         e.preventDefault();
+
+        const todoTitle = (document.querySelector('.add-input')).value;
+        const todoDetails = (document.querySelector('.add-textarea')).value;
+        const todoDueDate = (document.querySelector('.date-input')).value;
+        const todoPriority = (document.querySelector('[name="new-priority"]:checked')).value;
+        const todoProject = getSelectedProject();
+
+        const newTodo = createTodo(todoTitle, todoDetails, todoDueDate, todoPriority, todoProject);
+        //todoList[todoProject].push(newTodo);
     }
 })
