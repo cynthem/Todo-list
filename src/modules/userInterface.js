41948fd7348444115587bc626297329f2b523fdf
doozles411 <cindy.phelps01@gmail.com>
2022-08-18 12:35:09 -0700

    Add event listeners to popup cards

--- src/modules/userInterface.js
+++ src/modules/userInterface.js
@@ -1,15 +1,16 @@
+import { add } from "date-fns";
 import { changeDOM } from "./DOM";
 import { manageData } from "./storage";
 
+// All content container
+const contentContainer = document.getElementById('content');
 // Hamburger menu
 const hamburgerMenu = document.querySelector('.header-menu');
 // Filter buttons
-const filterAll = document.querySelector('.all-btn');
-const filterToday = document.querySelector('.today-btn');
-const filterWeek = document.querySelector('.week-btn');
+const filterBtns = document.querySelector('.filters-btn');
 const filterProject = document.querySelector('.projects-name');
 // Add-new button
-const projectsNew = document.querySelector('.projects-btn');
+const addNew = document.querySelector('.projects-btn');
 // Todos list container
 const listContainer = document.querySelector('.list');
 // Todos list item buttons
@@ -18,22 +19,29 @@ const itemNotes = document.querySelector('.item-notes');
 const itemEdit = document.querySelector('.item-edit');
 const itemDelete = document.querySelector('.item-delete');
 // Popup card buttons
-    // Empty project buttons
+const allExit = document.querySelector('fa-xmark');
+    // Empty project card
+const emptyCard = document.querySelector('.empty-project-card');
 const emptyExit = document.getElementById('empty-cancel');
 const emptyAdd = document.querySelector('.empty-add');
 const emptyDelete = document.querySelector('.empty-delete');
-    // Add-new form buttons
+    // Add-new card
+const addNewCard = document.querySelector('.add-new-card');
 const addExit = document.querySelector('.add-cancel');
 const addTodo = document.querySelector('.add-btn-todo');
+const addTodoDisplay = document.querySelector('.add-todo-form');
 const addProject = document.querySelector('.add-btn-project');
+const addProjectDisplay = document.querySelector('.add-project-form');
 const addLowPriority = document.getElementById('new-low');
 const addMediumPriority = document.getElementById('new-medium');
 const addHighPriority = document.getElementById('new-high');
 const addTodoSubmit = document.querySelector('.new-todo-submit');
 const addProjectSubmit = document.querySelector('.new-project-submit');
-    // Notes buttons
+    // Notes card
+const notesCard = document.querySelector('.notes-card');
 const notesExit = document.querySelector('.notes-cancel');
-    // Edit form buttons
+    // Edit card
+const editCard = document.querySelector('.edit-card');
 const editExit = document.querySelector('.edit-cancel');
 const editLowPriority = document.getElementById('edit-low');
 const editMediumPriority = document.getElementById('edit-medium');
@@ -45,14 +53,58 @@ const todos = JSON.parse(localStorage.getItem('todos')) || {
     'today': [],
     'week': [],
     'Kitchen renovation': []
-}
+};
 
 if (!localStorage.getItem('todos')) {
     todos['Kitchen renovation'].push(manageData.createTodo('Remove vinyl floor', 'replace with subfloor', '2022-08-24', 'high', 'Kitchen renovation'));
     todos['Kitchen renovation'].push(manageData.createTodo('Build cabinets', 'pre-painted cabinets only', '2022-08-26', 'low', 'Kitchen renovation', true));
     todos['Kitchen renovation'].push(manageData.createTodo('Install countertop', 'leave room for butcher block', '2022-09-18', 'medium', 'Kitchen renovation'));
     todos.all.push(manageData.createTodo('Finish book for book club', 'let Angela borrow afterward', '2022-08-24', 'high', 'all', true));
-}
+};
 
 changeDOM.renderAllTodos(todos, listContainer);
 changeDOM.renderProjectList(todos, listContainer);
+
+filterBtns.forEach(button => {
+    button.addEventListener('click', e => changeDOM.manageTodosRender(e, todos, listContainer));
+});
+
+allExit.addEventListener('click', () => {
+    if (allExit.parentElement.classList.contains('add-cancel')) {
+        addNewCard.style.display = 'none';
+        contentContainer.classList.remove('blur');
+    } else if (allExit.parentElement.classList.contains('empty-project-card')) {
+        emptyCard.style.display = 'none';
+        contentContainer.classList.remove('blur');
+    } else if (allExit.parentElement.classList.contains('notes-cancel')) {
+        notesCard.style.display = 'none';
+        contentContainer.classList.remove('blur');
+    } else if (allExit.parentElement.classList.contains('edit-cancel')) {
+        editCard.style.display = 'none';
+        contentContainer.classList.remove('blur');
+    }
+});
+
+addNew.addEventListener('click', () => {
+    contentContainer.classList.add('blur');
+    addNewCard.style.display = 'flex';
+})
+
+addExit.addEventListener('click', () => {
+    addNewCard.style.display = 'none';
+    contentContainer.classList.remove('blur');
+})
+
+addTodo.addEventListener('click', () => {
+    addTodo.classList.add('clicked');
+    addTodoDisplay.style.display = 'grid';
+    addProject.classList.remove('clicked');
+    addProjectDisplay.style.display = 'none';
+})
+
+addProject.addEventListener('click', () => {
+    addProject.classList.add('clicked');
+    addProjectDisplay.style.display = 'grid';
+    addTodo.classList.remove('clicked');
+    addTodoDisplay.style.display = 'none';
+})
