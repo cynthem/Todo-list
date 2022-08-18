8233b849fb4f4ddca6a3adcae89605e6b78bf6a4
doozles411 <cindy.phelps01@gmail.com>
2022-08-18 11:37:06 -0700

    Create todos object

--- src/modules/userInterface.js
+++ src/modules/userInterface.js
@@ -1,3 +1,5 @@
+import { manageData } from "./storage";
+
 // Hamburger menu
 const hamburgerMenu = document.querySelector('.header-menu');
 // Filter buttons
@@ -7,12 +9,18 @@ const filterWeek = document.querySelector('.week-btn');
 const filterProject = document.querySelector('.projects-name');
 // Add-new button
 const projectsNew = document.querySelector('.projects-btn');
+// Todos list container
+const listContainer = document.querySelector('.list');
 // Todos list item buttons
 const itemCheckbox = document.querySelector('.item-check');
 const itemNotes = document.querySelector('.item-notes');
 const itemEdit = document.querySelector('.item-edit');
 const itemDelete = document.querySelector('.item-delete');
 // Popup card buttons
+    // Empty project buttons
+const emptyExit = document.getElementById('empty-cancel');
+const emptyAdd = document.querySelector('.empty-add');
+const emptyDelete = document.querySelector('.empty-delete');
     // Add-new form buttons
 const addExit = document.querySelector('.add-cancel');
 const addTodo = document.querySelector('.add-btn-todo');
@@ -31,6 +39,18 @@ const editMediumPriority = document.getElementById('edit-medium');
 const editHighPriority = document.getElementById('edit-high');
 const editSubmit = document.querySelector('.edit-submit');
 
+const todos = JSON.parse(localStorage.getItem('todos')) || {
+    'all': [],
+    'today': [],
+    'week': [],
+    'Kitchen renovation': []
+}
 
+if (!localStorage.getItem('todos')) {
+    todos['Kitchen renovation'].push(manageData.createTodo('Remove vinyl floor', 'replace with subfloor', '2022-08-24', 'high', 'Kitchen renovation'));
+    todos['Kitchen renovation'].push(manageData.createTodo('Build cabinets', 'pre-painted cabinets only', '2022-08-26', 'low', 'Kitchen renovation', true));
+    todos['Kitchen renovation'].push(manageData.createTodo('Install countertop', 'leave room for butcher block', '2022-09-18', 'medium', 'Kitchen renovation'));
+    todos.all.push(manageData.createTodo('Finish book for book club', 'let Angela borrow afterward', '2022-08-24', 'high', 'all', true));
+}
 
 
